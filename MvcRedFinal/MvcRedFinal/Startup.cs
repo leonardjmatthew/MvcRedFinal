b4a4e0047afe55ae3b6c847f9b94b06ea330e9bd
leonardjmatthew <leonardjmatthew@gmail.com>
2022-03-31 10:47:23 -0400

    created structure

---
+++ MvcRedFinal/MvcRedFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcRedFinal.Startup))]
+namespace MvcRedFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
